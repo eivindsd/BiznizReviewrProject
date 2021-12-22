79c1ba1a97c01ad32c0b912aff5a9f2cc0862f40
Eivind Syrdalen Dovland <56222971+eivindsd@users.noreply.github.com>
2021-12-22 13:41:25 +0100

    init

---
+++ src/main/java/it/unipi/lsmsdb/biznizreviewrproject/BiznizReviewrProjectApplication.java
@@ -0,0 +1,13 @@
+package it.unipi.lsmsdb.biznizreviewrproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiznizReviewrProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiznizReviewrProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unipi/lsmsdb/biznizreviewrproject/BiznizReviewrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.lsmsdb.biznizreviewrproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiznizReviewrProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
