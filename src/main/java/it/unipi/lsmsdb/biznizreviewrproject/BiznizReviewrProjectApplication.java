480817aa942fd74c223008dd16854a6389d2ef03
Eivind Syrdalen Dovland <56222971+eivindsd@users.noreply.github.com>
2021-12-30 17:45:06 +0100

    removed unused commandlinerunner

--- src/main/java/it/unipi/lsmsdb/biznizreviewrproject/BiznizReviewrProjectApplication.java
+++ src/main/java/it/unipi/lsmsdb/biznizreviewrproject/BiznizReviewrProjectApplication.java
@@ -1,18 +1,13 @@
 package it.unipi.lsmsdb.biznizreviewrproject;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BiznizReviewrProjectApplication implements CommandLineRunner {
+public class BiznizReviewrProjectApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(BiznizReviewrProjectApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-    }
 }
