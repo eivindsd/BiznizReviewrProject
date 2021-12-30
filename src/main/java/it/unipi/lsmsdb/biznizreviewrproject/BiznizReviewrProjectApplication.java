85d39cd9d8e3bec02717371e72daa055d4bdbf02
eirikolav <eirik.olav.aa@gmail.com>
2021-12-30 09:32:48 +0100

    Setup fror Business CRUD API

--- src/main/java/it/unipi/lsmsdb/biznizreviewrproject/BiznizReviewrProjectApplication.java
+++ src/main/java/it/unipi/lsmsdb/biznizreviewrproject/BiznizReviewrProjectApplication.java
@@ -1,13 +1,18 @@
 package it.unipi.lsmsdb.biznizreviewrproject;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BiznizReviewrProjectApplication {
+public class BiznizReviewrProjectApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BiznizReviewrProjectApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
---
+++ src/main/java/it/unipi/lsmsdb/biznizreviewrproject/model/Business.java
@@ -0,0 +1,94 @@
+package it.unipi.lsmsdb.biznizreviewrproject.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document("business")
+public class Business {
+    @Id
+    private String _id;
+
+    private String businessid;
+    private String name;
+    private String city;
+    private String state;
+    private int stars;
+    private List<String> categories;
+    private List<Review> reviews = new ArrayList<>();
+
+    public Business(String businessid, String name, String city, String state, int stars, List<String> categories) {
+        this.businessid = businessid;
+        this.name = name;
+        this.city = city;
+        this.state = state;
+        this.stars = stars;
+        this.categories = categories;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getBusinessid() {
+        return businessid;
+    }
+
+    public void setBusinessid(String businessid) {
+        this.businessid = businessid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+}
